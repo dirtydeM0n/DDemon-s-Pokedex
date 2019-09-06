46e3b61b3f333075a3f4dee24ec83c684c19103e
Siddiqui <31212069+dirtydeM0n@users.noreply.github.com>
2019-09-06 18:29:31 +0500

    API calls using javascript

--- app.js
+++ app.js
@@ -1,7 +1,26 @@
-console.log('Hello world');
+console.log("Welcome to DDemon's Pokedex v1.0");
 
 const fetchPokemon = () => {
-  const url = `https://pokeapi.co/api/v2/pokemon/`;
-};
+  const promises = [];
+  for (let i = 1; i <= 10; i++) {
+    const url = `https://pokeapi.co/api/v2/pokemon/${i}`;
 
+    promises.push(fetch(url).then(res => res.json()));
+  }
+  Promise.all(promises).then(results => {
+    const pokemon = results.map(data => ({
+      name: data.name,
+      id: data.id,
+      image: data.sprites['front_default'],
+      type: data.types.map(type => type.type.name).join(', ')
+    }));
+    displayPokemon(pokemon);
+    // data.types.forEach(type => {
+    //   pokemon['type'] = pokemon['type'] + ', ' + type.type.name;
+    // });
+  });
+};
+const displayPokemon = pokemon => {
+  console.log(pokemon);
+};
 fetchPokemon();
